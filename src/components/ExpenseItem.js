adaf7a3d881d0383553ee70e5b46d58a76566550
Shantanub05 <105590952+Shantanub05@users.noreply.github.com>
2023-05-31 09:59:52 +0530

    ExpenseItem changed

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,7 +3,11 @@ import React from 'react'
 const ExpenseItem = () => {
   return (
     <div>
-      <p>lorem</p>
+      <div>Date</div>
+      <div>
+        <h2>Title</h2>
+        <div>Amount</div>
+      </div>
     </div>
   )
 }
